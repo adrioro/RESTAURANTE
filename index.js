66068d821d18c6109ec39e89a6cf7446d7782be7
adrioro <artistsvillage@me.com>
2019-03-13 17:33:59 -0400

    Commit #60

--- index.js
+++ index.js
@@ -48,3 +48,13 @@ function MoveUpArrow() {
     });
 };
 MoveUpArrow();
+
+/*NAV Menu*/
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
